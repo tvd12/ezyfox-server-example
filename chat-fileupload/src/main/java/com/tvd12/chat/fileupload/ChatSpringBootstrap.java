13cbf44c17763b4352f44f3486b905f23336aa31
Dung Ta Van <itprono3@gmail.com>
2017-09-14 09:48:04 +0700

    update bootstrap

--- chat-fileupload/src/main/java/com/tvd12/chat/fileupload/ChatSpringBootstrap.java
+++ chat-fileupload/src/main/java/com/tvd12/chat/fileupload/ChatSpringBootstrap.java
@@ -3,18 +3,19 @@ package com.tvd12.chat.fileupload;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.tvd12.ezyfoxserver.entity.EzyDestroyable;
 import com.tvd12.ezyfoxserver.entity.EzyStartable;
-import com.tvd12.ezyfoxserver.util.EzyLoggable;
 
 @ComponentScan
 @SpringBootApplication
 @EnableAutoConfiguration
 public class ChatSpringBootstrap 
-		extends EzyLoggable implements EzyStartable, EzyDestroyable {
+		extends SpringBootServletInitializer 
+		implements EzyStartable, EzyDestroyable {
 
 	protected ConfigurableApplicationContext applicationContext;
 	
