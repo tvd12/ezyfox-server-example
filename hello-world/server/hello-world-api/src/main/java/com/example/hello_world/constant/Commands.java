9022870dd3a289b4606540c8a4ac99ed45b8790c
Dung Ta Van <itprono3@gmail.com>
2020-03-29 13:28:47 +0700

    update hello: add udp

--- hello-world/server/hello-world-api/src/main/java/com/example/hello_world/constant/Commands.java
+++ hello-world/server/hello-world-api/src/main/java/com/example/hello_world/constant/Commands.java
@@ -3,8 +3,9 @@ package com.example.hello_world.constant;
 
 public final class Commands {
 
-	public static final String GREET = "greet";
-	public static final String BROADCAST_MESSAGE = "broadcastMessage";
+	public static final String GREET 				= "greet";
+	public static final String BROADCAST_MESSAGE 	= "broadcastMessage";
+	public static final String UDP_GREET 			= "udpGreet";
 	
 	private Commands() {
 	}
