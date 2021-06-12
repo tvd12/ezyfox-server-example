f92aaf985a5369bfc19bca359923daa6b9716f4c
Dung Ta Van <itprono3@gmail.com>
2021-06-12 22:07:59 +0700

    update hello-world

--- hello-world/server/hello-world-plugin/src/main/java/com/example/hello_world/plugin/controller/UserLoginController.java
+++ hello-world/server/hello-world-plugin/src/main/java/com/example/hello_world/plugin/controller/UserLoginController.java
@@ -12,7 +12,7 @@ public class UserLoginController extends EzyAbstractPluginEventController<EzyUse
 
 	@Override
 	public void handle(EzyPluginContext ctx, EzyUserLoginEvent event) {
-		getLogger().info("handle user {} login in", event.getUsername());
+		logger.info("handle user {} login in", event.getUsername());
 	}
 	
 }
