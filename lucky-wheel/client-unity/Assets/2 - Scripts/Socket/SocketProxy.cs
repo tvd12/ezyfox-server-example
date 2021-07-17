5f3163d53a50c2aef1dad5e14bde26ac4ab95ada
vu-luong <vubinhcht@gmail.com>
2021-07-17 22:54:15 +1000

    Init Socket component

---
+++ lucky-wheel/client-unity/Assets/2 - Scripts/Socket/SocketProxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class SocketProxy {
+
+    private static SocketProxy _instance;
+
+    public static SocketProxy getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new SocketProxy();
+        }
+
+        return _instance;
+    }
+}
