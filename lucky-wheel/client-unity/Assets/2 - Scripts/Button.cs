e78a88f3a0d86eabe739ec2453c2410b225c779b
vu-luong <vubinhcht@gmail.com>
2021-07-17 22:34:51 +1000

    Implement observer pattern for click event

---
+++ lucky-wheel/client-unity/Assets/2 - Scripts/Button.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+	public delegate void ClickDelegate();
+	public event ClickDelegate clickEvent;
+
+	public void Click()
+	{
+		clickEvent?.Invoke();
+	}
+
+}
