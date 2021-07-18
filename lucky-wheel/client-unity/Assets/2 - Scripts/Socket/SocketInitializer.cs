92e228100323630d192442bddb35becd47f8dc7f
vu-luong <vubinhcht@gmail.com>
2021-07-18 22:10:25 +1000

    Refactor: add spin controller

---
+++ lucky-wheel/client-unity/Assets/2 - Scripts/Socket/SocketInitializer.cs
@@ -0,0 +1,31 @@
+﻿using com.tvd12.ezyfoxserver.client;
+using com.tvd12.ezyfoxserver.client.logger;
+using UnityEngine;
+
+public class SocketInitializer : MonoBehaviour
+{
+    private EzyClient client;
+    private EzyLogger logger;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Enable EzyLogger
+        EzyLoggerFactory.setLoggerSupply(type => new UnityLogger(type));
+        logger = EzyLoggerFactory.getLogger<SocketInitializer>();
+
+        // Set up socket client
+        var socketProxy = SocketProxy.getInstance();
+        client = socketProxy.setup();
+
+        // Connect to socket server
+        client.connect("127.0.0.1", 3005);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Main thread pulls data from socket
+        client.processEvents();
+    }
+}
---
+++ lucky-wheel/client-unity/Assets/2 - Scripts/Socket/SpinController.cs
@@ -0,0 +1,29 @@
+﻿using com.tvd12.ezyfoxserver.client;
+using com.tvd12.ezyfoxserver.client.logger;
+using UnityEngine;
+
+public class SpinController : MonoBehaviour
+{
+    public GameObject spinButton;
+    public GameObject wheel;
+
+    private void Awake()
+    {
+        spinButton.GetComponent<Button>().clickEvent += SpinButtonClick;
+
+        SpinResponseHandler.spinResponseEvent += SpinResponse;
+    }
+
+    void SpinButtonClick()
+    {
+        if (!wheel.GetComponent<RotateSelf>().Enable)
+        {
+            SocketRequest.getInstance().SendSpinRequest();
+        }
+    }
+
+    void SpinResponse(int result)
+    {
+        wheel.GetComponent<RotateSelf>().Activate(result);
+    }
+}
