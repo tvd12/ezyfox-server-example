0bbe3aac3ce6e21fde36148863e1f68eb4ba2dcc
vu-luong <vubinhcht@gmail.com>
2021-07-25 13:16:03 +1000

    Update prize quantity

--- lucky-wheel/src/main/java/com/tvd12/example/lucky_wheel/repo/WheelRepo.java
+++ lucky-wheel/src/main/java/com/tvd12/example/lucky_wheel/repo/WheelRepo.java
@@ -1,7 +1,10 @@
 package com.tvd12.example.lucky_wheel.repo;
 
 import com.tvd12.example.lucky_wheel.entity.Wheel;
+import com.tvd12.ezydata.mongodb.EzyMongoRepository;
+import com.tvd12.ezyfox.database.annotation.EzyRepository;
 
-public interface WheelRepo {
-    Wheel findById(long id);
+@EzyRepository("wheelRepo")
+public interface WheelRepo extends EzyMongoRepository<String, Wheel> {
+
 }
