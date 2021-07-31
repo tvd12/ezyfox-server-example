72ea2e1bae5eff7cb2557ed43c107060f93bfcb1
Dung Ta Van <itprono3@gmail.com>
2021-07-31 18:50:04 +0700

    add csharpt

---
+++ push-message/api-gateway-csharp/push_message_api/Controllers/MessageController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Confluent.Kafka;
+using MessagePack;
+using System.Net;
+
+namespace push_message_api.Controllers
+{
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly ILogger<MessageController> _logger;
+        private readonly IProducer<String, byte[]> _producer;
+
+        public MessageController(ILogger<MessageController> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                ClientId = Dns.GetHostName()
+            };
+            _producer = new ProducerBuilder<String, byte[]>(config)
+                .SetValueSerializer(new DefaultKafkaSerializer())
+                .Build();
+        }
+
+        [HttpPost]
+        [Route("api/v1/message/push")]
+        public Boolean Push(MessagePushModel request)
+        {
+            byte[] messageBytes = MessagePackSerializer.Serialize(request);
+            _producer.Produce(
+                "message",
+                new Message<String, byte[]> { Key = "push", Value = messageBytes });
+            return true;
+        }
+    }
+
+    public class DefaultKafkaSerializer : ISerializer<byte[]>
+    {
+        public byte[] Serialize(byte[] data, SerializationContext context)
+        {
+            return data;
+        }
+    }
+}
---
+++ push-message/api-gateway-csharp/push_message_api/MessagePushModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MessagePack;
+
+namespace push_message_api
+{
+    [MessagePackObject]
+    public class MessagePushModel
+    {
+        [Key("username")]
+        public String username { get; set; }
+
+        [Key("data")]
+        public MessagePushData data { get; set; }
+    }
+
+    [MessagePackObject]
+    public class MessagePushData
+    {
+        [Key("title")]
+        public String title { get; set; }
+
+        [Key("content")]
+        public String content { get; set; }
+    }
+}
