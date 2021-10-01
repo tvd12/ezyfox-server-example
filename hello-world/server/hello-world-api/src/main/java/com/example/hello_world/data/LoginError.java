cb5acd352d59a50c18c7ae0113d78ffd3fbf9dea
Dung Ta Van <itprono3@gmail.com>
2021-10-01 23:49:25 +0700

    add csharp example

---
+++ hello-world/server/hello-world-api/src/main/java/com/example/hello_world/data/LoginError.java
@@ -0,0 +1,21 @@
+package com.example.hello_world.data;
+
+import com.tvd12.ezyfoxserver.constant.EzyILoginError;
+
+import lombok.Getter;
+
+@Getter
+public enum LoginError implements EzyILoginError {
+
+	PASSWORD_TOO_LONG(100, "password too long"),
+	PASSWORD_TOO_SHORT(101, "password too short");
+	
+	private final int id;
+	private final String message;
+	
+	private LoginError(int id, String message) {
+		this.id = id;
+		this.message = message;
+	}
+	
+}
