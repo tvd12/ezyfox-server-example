cc78efa0302d12cfdd326d3e7f70ad5657df0dde
Dung Ta Van <itprono3@gmail.com>
2022-05-06 22:58:23 +0700

    add scripts

---
+++ hello-world/server/export.sh
@@ -0,0 +1,5 @@
+mvn clean install
+mvn -pl hello-world-api -Pexport clean install
+mvn -pl hello-world-common -Pexport clean install
+mvn -pl hello-world-entry -Pexport clean install
+mvn -pl hello-world-plugin -Pexport clean install
