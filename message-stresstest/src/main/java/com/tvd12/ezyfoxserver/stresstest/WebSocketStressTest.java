816c39de0a24ea94cf551318b19eb22eb866ef58
Dung Ta Van <itprono3@gmail.com>
2023-07-25 23:31:53 +0700

    update stress test

--- message-stresstest/src/main/java/com/tvd12/ezyfoxserver/stresstest/WebSocketStressTest.java
+++ message-stresstest/src/main/java/com/tvd12/ezyfoxserver/stresstest/WebSocketStressTest.java
@@ -1,42 +1,44 @@
 package com.tvd12.ezyfoxserver.stresstest;
 
 import com.tvd12.ezyfox.concurrent.EzyEventLoopGroup;
-import com.tvd12.ezyfoxserver.client.EzyClients;
+import com.tvd12.ezyfoxserver.client.EzyClient;
 import com.tvd12.ezyfoxserver.client.EzyWsClient;
-import com.tvd12.ezyfoxserver.client.concurrent.EzyNettyEventLoopGroup;
-import com.tvd12.ezyfoxserver.client.socket.EzyMainEventsLoop;
+import com.tvd12.ezyfoxserver.client.config.EzyClientConfig;
 import io.netty.channel.EventLoopGroup;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class WebSocketStressTest {
+public class WebSocketStressTest extends SocketStressTest {
 
     public static void main(String[] args) {
-        DefaultClientConfig clientConfig = new DefaultClientConfig();
-        SocketClientSetup setup = new SocketClientSetup("websocket");
-        EzyClients clients = EzyClients.getInstance();
-        EzyEventLoopGroup eventLoopGroup = new EzyEventLoopGroup(16);
-        EventLoopGroup nettyEventLoopGroup = new EzyNettyEventLoopGroup(16);
-        new Thread(() -> {
-            int clientCount = 300;
-            for (int i = 0; i < clientCount; i++) {
-                EzyWsClient client = new EzyWsClient(
-                    clientConfig.get(i),
-                    eventLoopGroup,
-                    nettyEventLoopGroup
-                );
-                try {
-                    Thread.sleep(25);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                setup.setup(client, false);
-                clients.addClient(client);
-                client.connect("ws://127.0.0.1:2208/ws");
-            }
-        })
-            .start();
-        EzyMainEventsLoop mainEventsLoop = new EzyMainEventsLoop();
-        mainEventsLoop.start(5);
+        new WebSocketStressTest().test();
+    }
+
+    @Override
+    protected int clientCount() {
+        return 100;
+    }
+
+    @Override
+    protected int testDurationInSecond() {
+        return 5;
+    }
+
+    @Override
+    protected EzyClient newClient(
+        EzyClientConfig config,
+        EzyEventLoopGroup eventLoopGroup,
+        EventLoopGroup nettyEventLoopGroup
+    ) {
+        return new EzyWsClient(
+            config,
+            eventLoopGroup,
+            nettyEventLoopGroup
+        );
+    }
+
+    @Override
+    protected void connect(EzyClient client) {
+        client.connect("ws://127.0.0.1:2208/ws");
     }
 }
